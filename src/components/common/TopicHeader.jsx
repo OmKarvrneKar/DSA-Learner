aef69d6833204248fc992b483d654d143e6a6a92
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-07-14 00:18:05 +0530

    feat: add TopicHeader component with completion toggle and quiz integration

--- src/components/common/TopicHeader.jsx
+++ src/components/common/TopicHeader.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useApp } from '../context/AppContext'
+import { useApp } from '../../context/AppContext'
 import QuizModal from './QuizModal'
 
 export default function TopicHeader({ topic, title, subtitle, hasQuiz = true }) {
